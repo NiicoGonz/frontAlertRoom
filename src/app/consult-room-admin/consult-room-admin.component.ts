8d960f4165bf88b9f6f5a27cf975e97a6be2c852
Niico <nicolas.gonz.mart@gmail.com>
2020-12-03 17:09:14 -0500

    corrigiendo interfaces admin

--- src/app/consult-room-admin/consult-room-admin.component.ts
+++ src/app/consult-room-admin/consult-room-admin.component.ts
@@ -79,29 +79,37 @@ export class ConsultRoomAdminComponent implements OnInit {
     }
   }
   getAmbiente(): void {
-    this.client
-      .getRequest(
-        `http://alertroomws.herokuapp.com/api/ambientes/consultar/${this.consultar.value.numero}`
-      )
-      .subscribe((response: any) => {
-        this.toast = true;
-        this.arrayAmbiente = response.body['listaInventarioAmbiente'];
-        this.arrayObjetos = response.body.listaInventarioAmbiente;
-        this.estado = response.body.estado;
-        console.log(this.estado);
-        this.nombreAmbiente = response.body.nombre;
-        if (response.body.estado === 'A') {
-          this.estado = 'Activo';
-        }
-        this.numeroAmbiente = response.body.id;
-        this.notification();
-      }),
-      // tslint:disable-next-line: no-unused-expression
-      (error) => {
-        if ((error.status = 404)) {
-          this.toast = false;
+    if (this.form.valid) {
+      this.client
+        .getRequest(
+          `http://alertroomws.herokuapp.com/api/ambientes/consultar/${this.consultar.value.numero}`
+        )
+        .subscribe((response: any) => {
+          this.toast = true;
+          this.arrayAmbiente = response.body['listaInventarioAmbiente'];
+          this.arrayObjetos = response.body.listaInventarioAmbiente;
+          this.estado = response.body.estado;
+          console.log(this.estado);
+          this.nombreAmbiente = response.body.nombre;
+          if (response.body.estado === 'A') {
+            this.estado = 'Activo';
+          }
+          this.numeroAmbiente = response.body.id;
           this.notification();
-        }
-      };
+        }),
+        // tslint:disable-next-line: no-unused-expression
+        (error) => {
+          if (error.status === 404) {
+            this.toast = false;
+            this.notification();
+          }
+        };
+    }else{
+       Swal.fire({
+         title: 'Recuerde llenar los campos obligatorios!',
+         confirmButtonText: 'ok!',
+         confirmButtonColor: 'rgb(252, 116, 5) ',
+       });
+    }
   }
 }
